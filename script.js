da55a85045294ceadc368e5068a2181f77ad8444
PRINCE-OBOT <princessamuel45@gmail.com>
2024-12-04 21:28:33 +0100

    Add function Add opacity
    The function returns random
    background colors.
    
    Add opacity to control the saturation
    pf each clicked box.
    
    background

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const container = document.querySelector(".container")
 const preferredChoice = document.querySelector(".preferredChoice")
 
+let opacityIncreement = 0
 let execute = 0
+
+function randBackgroundColor(num){
+  return Math.floor(Math.random()*num)
+}
 
 let dispatchEtch = new Event("click")
 
 preferredChoice.addEventListener("click", (e)=>{
-  let numberOfEtch = 2
+  let numberOfEtch = 16
   execute++
   
   if(execute > 1){
@@ -18,6 +23,21 @@ for(i=1; i<=numberOfEtch; i++){
    for(j=1; j<=numberOfEtch; j++){
     const containerGrandChild = document.createElement("div")
     
+    containerGrandChild.addEventListener("click", (e)=>{
+      let clickedArea = e.target
+      let saturatedBackground =
+  ` background : rgb(${randBackgroundColor(255)+1}, ${randBackgroundColor(255)+1}, ${randBackgroundColor(255)+1});
+     opacity : 0.${opacityIncreement};`
+     
+     
+      if(clickedArea){
+        if(opacityIncreement === 9){
+          opacityIncreement = 0
+        }
+        opacityIncreement++
+        clickedArea.style.cssText = saturatedBackground
+      }
+    })
     containerChild.appendChild(containerGrandChild)
     
     containerGrandChild.classList.toggle("containerGrandChildToggle")
