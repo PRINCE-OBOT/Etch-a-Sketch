7dc31826bb5a9cfce2a28f39462bee5cfc6286a9
PRINCE-OBOT <princessamuel45@gmail.com>
2024-12-04 19:24:33 +0100

    Add nested loop
    This will directly append the
    grandchildren container in each
    child container.

--- script.js
+++ script.js
@@ -15,15 +15,26 @@ preferredChoice.addEventListener("click", (e)=>{
 
 for(i=1; i<=numberOfEtch; i++){
   const containerChild = document.createElement("div")
+   for(j=1; j<=numberOfEtch; j++){
+    const containerGrandChild = document.createElement("div")
+    
+    containerChild.appendChild(containerGrandChild)
+    
+    containerGrandChild.classList.toggle("containerGrandChildToggle")
+  }
+  
   container.appendChild(containerChild)
+  
+  containerChild.classList.toggle("containerChildToggle")
+  
   }
 
   function enterPreferredChoice(){
 let enteredChoice = +prompt(`Enter the number of Etch-a-Sketch you want ranging from (1 - 200)`) 
 if(enteredChoice){
- const divs = document.querySelectorAll("div")
+ const containerChild = document.querySelectorAll(".containerChildToggle")
  
- divs.forEach((div)=>{
+ containerChild.forEach((div)=>{
   container.removeChild(div)
        })
     }
