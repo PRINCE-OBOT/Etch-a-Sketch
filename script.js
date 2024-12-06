a5263362a958e5b8c4a7b26c0615735ae5f2bcee
PRINCE-OBOT <princessamuel45@gmail.com>
2024-12-06 22:20:32 +0100

    Add transition and modify event listener
    Positioning the add event listener on the document
    body to enable it other event listener access the
    updated value.

--- script.js
+++ script.js
@@ -1,127 +1,111 @@
-const container = document.querySelector(".container")
-const headText = document.querySelector("h3")
-const backgroundColorCon = document.querySelector(".backgroundColorCon")
-const headerContainer = document.querySelector(".headerContainer")
-const dimension = document.querySelector(".dimension")
+const container = document.querySelector(".container");
+const headText = document.querySelector("h3");
+const backgroundColorCon = document.querySelector(".backgroundColorCon");
+const dimension = document.querySelector(".dimension");
 
-let opacityIncreement = 0
-let execute = 0
+let opacityIncreement = 9;
 let defaultColor = "defColor";
-let opacitySetting = true;
+let opacitySetting = false;
+let defaultNumberOfEtch = 16;
+let generateGrid = 0;
 
-function randBackgroundColor(rgb){
-  if(defaultColor === "defColor"){
-    return 0
-  }
-  else if(defaultColor === "mulColor"){
-    return Math.floor(Math.random()*rgb)
-  }
-  else if(defaultColor === "singleColor"){
-    return 10
+function backgroundSelection(rgb) {
+  if (defaultColor === "defColor") {
+    return 0;
+  } else if (defaultColor === "mulColor") {
+    return Math.floor(Math.random() * rgb);
+  } else if (defaultColor === "singleColor") {
+    return 10;
   }
 }
 
-function opacity(range){
-  if(opacityIncreement === 9){
-    return 9
-  }
-  else if(opacityIncreement <= 10){
-  return range
+function opacity(range) {
+  if (opacityIncreement === 9) {
+    return 9;
+  } else if (opacityIncreement <= 10) {
+    return range;
   }
 }
-dispatchDefaultEtchNum()
-function dispatchDefaultEtchNum(){
-let dispatchEtch = new Event("click")
 
-headerContainer.addEventListener("click", (e)=>{
+container.addEventListener("mouseenter", (event) => {
+  if (event.target) generateGrid++;
+  if (generateGrid === 1) {
+    for (i = 1; i <= defaultNumberOfEtch; i++) {
+      const containerChild = document.createElement("div");
+      for (j = 1; j <= defaultNumberOfEtch; j++) {
+        const containerGrandChild = document.createElement("div");
 
-//Updates the value of opacityIncreement, opacitySetting when clicked 
-  opacityIncreement = 9
-  opacitySetting = false
-  let headerChild = e.target.className
-  let defaultNumberOfEtch = 16
-  execute++
-  
-  
-  //Enable the enterPreferredChoice function, prompt only after the default 16x16 grid display. Setting execute to 0, dispatching Event making it 1, and clicking the target making it 2, which is greater than 1, making the entered function to prompt.
-  if(execute > 1 && headerChild === "dimension"){
-    defaultNumberOfEtch = enterPreferredChoice()
-  }
-  //Updates the value of opacityIncreement, opacitySetting and defaultColor when clicked 
-  else if(execute > 1 && headerChild === "headText"){
-    defaultColor = defColor
-    opacityIncreement = 9;
-    opacity = false
-  }
+        containerGrandChild.addEventListener("mouseenter", (e) => {
+          let clickedArea = e.target;
+          let randomBackgroundColor = ` background : rgb(${backgroundSelection(
+            255
+          )}, ${backgroundSelection(255)}, ${backgroundSelection(255)});
+      opacity : 0.${opacity(opacityIncreement)}`;
 
-generateEnteredChoice()
-function generateEnteredChoice(){
- 
-for(i=1; i<=defaultNumberOfEtch; i++){
-  const containerChild = document.createElement("div")
-   for(j=1; j<=defaultNumberOfEtch; j++){
-    const containerGrandChild = document.createElement("div")
-    
-    containerGrandChild.addEventListener("click", (e)=>{
-      let clickedArea = e.target
-      let randomBackgroundColor =
-  ` background : rgb(${randBackgroundColor(255)}, ${randBackgroundColor(255)}, ${randBackgroundColor(255)});
-    opacity : 0.${opacity(opacityIncreement)}`
-     
-     
-      if(clickedArea){
-        if(opacityIncreement === 10){
-          opacityIncreement = 0
-        }
-        if(opacitySetting){
-        opacityIncreement++
-        }
-        clickedArea.style.cssText = randomBackgroundColor
+          if (clickedArea) {
+            if (opacityIncreement === 10) {
+              opacityIncreement = 0;
+            }
+            if (opacitySetting) {
+              opacityIncreement++;
+            }
+            clickedArea.style.cssText = randomBackgroundColor;
+          }
+        });
+        containerChild.appendChild(containerGrandChild);
+
+        containerGrandChild.classList.toggle("containerGrandChildToggle");
       }
-  
-    })
-    containerChild.appendChild(containerGrandChild)
-    
-    containerGrandChild.classList.toggle("containerGrandChildToggle")
-  }
-  
-  container.appendChild(containerChild)
-  
-  containerChild.classList.toggle("containerChildToggle")
-  }
-}
-  function enterPreferredChoice(){
-let enteredChoice = +prompt(`Enter the number of Etch-a-Sketch you want ranging from (1 - 100)`) 
-if(enteredChoice <= 100 && enteredChoice > 0){
- const containerChild = document.querySelectorAll(".containerChildToggle")
- 
- containerChild.forEach((div)=>{
-  container.removeChild(div)
-       })
-    dimension.textContent = `${enteredChoice}x${enteredChoice}`
-    return enteredChoice
+
+      container.appendChild(containerChild);
+
+      containerChild.classList.toggle("containerChildToggle");
     }
-    else if(enteredChoice > 100){ alert('Etch-a-Sketch number greater than 100') }
-   else if(enteredChoice == false){let cancel = true}
-    else if(enteredChoice <= 0){ alert('Etch-a-Sketch number lower than 0') }
-    else{ alert('Invalid Etch-a-Sketch input')} 
   }
-})
-
-headerContainer.dispatchEvent(dispatchEtch)
+});
+function enterPreferredChoice() {
+  let enteredChoice = +prompt(
+    `Enter the number of Etch-a-Sketch you want ranging from (1 - 100)`
+  );
+  if (enteredChoice <= 100 && enteredChoice > 0) {
+    const containerChild = document.querySelectorAll(".containerChildToggle");
+    containerChild.forEach((div) => {
+      container.removeChild(div);
+    });
+    defaultNumberOfEtch = enteredChoice;
+    dimension.innerHTML = `&#128297;${enteredChoice}x${enteredChoice}`;
+  } else if (enteredChoice > 100) {
+    alert("Etch-a-Sketch number greater than 100");
+  } else if (enteredChoice == false) {
+    let cancel = true;
+  } else if (enteredChoice <= 0) {
+    alert("Etch-a-Sketch number lower than 0");
+  } else {
+    alert("Invalid Etch-a-Sketch input");
+  }
 }
 
-//Updates the value of opacityIncreement,  opacitySetting and defaultColor when clicked 
-backgroundColorCon.addEventListener("click", (e)=>{
-  let clicked = e.target.className
-  if(clicked === "mulBgColor"){
+// dimension.addEventListener("click", ()=>{
+//     defaultNumberOfEtch = enterPreferredChoice();
+// })
+
+//Updates the value of opacityIncreement,  opacitySetting and defaultColor when clicked
+document.body.addEventListener("click", (e) => {
+  let clicked = e.target.className;
+  if (clicked === "mulBgColor") {
     defaultColor = "mulColor";
     opacityIncreement = 9;
-    opacitySetting = false
-  }
-  else if(clicked === "singleBackgroundColor"){
+    opacitySetting = false;
+  } else if (clicked === "singleBackgroundColor") {
     defaultColor = "singleColor";
-    opacityIncreement = 0
-    opacitySetting = true
+    opacityIncreement = 0;
+    opacitySetting = true;
+  } else if (clicked === "headText") {
+    defaultColor = "defColor";
+    opacityIncreement = 9;
+    opacitySetting = false;
+  } else if (clicked === "dimension") {
+    generateGrid = 0;
+    enterPreferredChoice();
   }
-})
+});
